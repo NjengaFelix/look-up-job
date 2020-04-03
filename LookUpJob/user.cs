6ba084f1181b64332bb2b70a16737ccbca26d4c9
Felix Njenga <32095327+NjengaFelix@users.noreply.github.com>
2020-04-03 13:46:03 +0300

    Added sign up and sign in file

---
+++ LookUpJob/user.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookUpJob
+{
+    class User
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string username;
+        private string password;
+        private bool is_staff;
+        private string dateJoined;
+
+        User(string firstName, string lastName, string email, string username, string password, bool is_staff, string dateJoined)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.username = username;
+            this.password = password;
+            this.is_staff = is_staff;
+            this.dateJoined = dateJoined;
+        }
+    }
+}
