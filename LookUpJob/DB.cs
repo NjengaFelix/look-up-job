b5d8dc920f4519824cdd1668d99c5288a77f11d6
Felix <felix@rocketmail.com>
2020-04-07 21:49:30 +0300

    User sign up

---
+++ LookUpJob/DB.cs
@@ -0,0 +1,64 @@
+﻿using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+[Table]
+public class User_details
+{
+    [Column(IsDbGenerated = true, IsPrimaryKey = true)]
+    public int id
+    {
+        get;
+        set;
+    }
+    [Column]
+    public string first_name
+    {
+        get;
+        set;
+    }
+    [Column]
+    public string last_name
+    {
+        get;
+        set;
+    }
+    [Column]
+    public string email
+    {
+        get;
+        set;
+    }
+    [Column]
+    public bool is_Employee_or_JobSeeker
+    {
+        get;
+        set;
+    }
+    [Column]
+    public string user_name
+    {
+        get;
+        set;
+    }
+    [Column]
+    public string password
+    {
+        get;
+        set;
+    }
+}
+
+public class UserDataContext : DataContext
+{
+    public static string DBConnectionString = @"isostore:/LookUpJobDb.sdf";
+    public UserDataContext(string connectionString)
+        : base(connectionString)
+    { }
+    public Table<User_details> Users
+    {
+        get
+        {
+            return this.GetTable<User_details>();
+        }
+    }
+}
--- LookUpJob/SignUpPage.xaml.cs
+++ LookUpJob/SignUpPage.xaml.cs
@@ -15,6 +15,47 @@ namespace LookUpJob
         public SignUpPage()
         {
             InitializeComponent();
+
+            using (UserDataContext context = new UserDataContext(UserDataContext.DBConnectionString))
+            {
+                if (!context.DatabaseExists())
+                    context.CreateDatabase();
+            }
         }
+
+        private void btnSignUpNow_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName, lastName, email, userName, password;
+            bool is_employee_or_jobSeeker = true;
+
+            firstName = txtFirstName.Text;
+            lastName = txtLastName.Text;
+            email = txtEmail.Text;
+            userName = txtUsername.Text;
+            password = txtPassword.Password;
+
+            addUser(firstName, lastName, email, is_employee_or_jobSeeker, userName, password);
+
+        }
+
+        private void addUser(string firstName, string lastName, string email, bool is_employee_or_jobSeeker, string userName, string password)
+        {
+            using (UserDataContext context = new UserDataContext(UserDataContext.DBConnectionString))
+            {
+                User_details ud = new User_details();
+                ud.first_name = firstName;
+                ud.last_name = lastName;
+                ud.email = email;
+                ud.is_Employee_or_JobSeeker = is_employee_or_jobSeeker;
+                ud.user_name = userName;
+                ud.password = password;
+
+                context.Users.InsertOnSubmit(ud);
+                context.SubmitChanges();
+            }
+        }
+
+
+
     }
 }
