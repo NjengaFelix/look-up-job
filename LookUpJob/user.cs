10943c640f820c4df6784d74556d1588b9a5d346
Felix <felix@rocketmail.com>
2020-04-13 15:16:33 +0300

    User class changed

--- LookUpJob/user.cs
+++ LookUpJob/user.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.IsolatedStorage;
 
 namespace LookUpJob
 {
@@ -11,20 +12,65 @@ namespace LookUpJob
         private string firstName;
         private string lastName;
         private string email;
+        private bool user_type;
         private string username;
         private string password;
-        private bool is_staff;
-        private string dateJoined;
 
-        User(string firstName, string lastName, string email, string username, string password, bool is_staff, string dateJoined)
+        public User() { }
+ 
+
+        //Insert user into the DB
+        public void createNewUser(string firstName, string lastName, string email, bool is_Employee_or_JobSeeker, string username, string password)
         {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.email = email;
-            this.username = username;
-            this.password = password;
-            this.is_staff = is_staff;
-            this.dateJoined = dateJoined;
+            using(UserDataContext udt = new UserDataContext(UserDataContext.DBConnectionString))
+            {
+                User_details ud = new User_details();
+                ud.first_name = firstName;
+                ud.last_name = lastName;
+                ud.email = email;
+                ud.is_Employee_or_JobSeeker = is_Employee_or_JobSeeker;
+                ud.user_name = username;
+                ud.password = password;
+
+                udt.Users.InsertOnSubmit(ud);
+                udt.SubmitChanges();
+
+            }
         }
+
+        //Load user profile
+        public IList<User_details> loadUserProfile(string username)
+        {
+            IList<User_details> list = null;
+            using(UserDataContext udt = new UserDataContext(UserDataContext.DBConnectionString))
+            {
+                IQueryable<User_details> query = from users in udt.Users where users.user_name == username select users;
+                list = query.ToList();
+            }
+            return list;
+        }
+
+
+        //Create IsolatedStorageSetting storage function
+        public void saveUsername(string usernameKey, string username)
+        {
+            IsolatedStorageSettings.ApplicationSettings[username] = usernameKey;
+            IsolatedStorageSettings.ApplicationSettings.Save();
+        }
+
+        //Return username from IsolatedStorageSetting
+        public static string loadUsername(string usernameKey)
+        {
+            if(IsolatedStorageSettings.ApplicationSettings.Contains(usernameKey))
+            {
+                return (string)IsolatedStorageSettings.ApplicationSettings[usernameKey];
+            }
+            else
+            {
+                //Login in
+                return null;
+            }
+        }
+        
     }
 }
