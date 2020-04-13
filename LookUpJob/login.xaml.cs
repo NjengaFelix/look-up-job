4d24c1947b4b7b0a4247a6680c583ef656dff404
Felix <felix@rocketmail.com>
2020-04-13 15:12:26 +0300

    Login updated

--- LookUpJob/login.xaml.cs
+++ LookUpJob/login.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
 
 namespace LookUpJob
 {
@@ -15,6 +16,66 @@ namespace LookUpJob
         public login()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string username, password;
+            bool user_type = false;
+            username = txtuser.Text;
+            password = txtpassword.Password;
+
+            //Validation
+            if (string.IsNullOrEmpty(username))
+            {
+                MessageBox.Show("Field username is empty!");
+                return;
+            }
+            else if (string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Field password is empty!");
+                return;
+            }
+            else
+            {
+                //Getting userName and password from DB
+                User user = new User();
+                IList<User_details> users = user.loadUserProfile(username);
+                if (users.Count == 0)
+                {
+                    //No list was returned
+                    MessageBox.Show("Username does not exist");
+                    return;
+                }
+                else
+                {
+
+                    string userPassword = "";
+                    foreach (User_details ud in users)
+                    {
+                        username = ud.user_name;
+                        userPassword = ud.password;
+                        user_type = ud.is_Employee_or_JobSeeker;
+                    }
+                    //Check password
+                    if (userPassword.Contains(password) == false)
+                    {
+                        MessageBox.Show("Incorrect password");
+                        return;
+                    }
+                    else if (userPassword.Contains(password))
+                    {
+                        //Create an IsolatedStorageSettings storage to save username and usertype (Session)
+                        IsolatedStorageSettings.ApplicationSettings["username"] = username;
+                        IsolatedStorageSettings.ApplicationSettings["user_type"] = user_type;
+                        IsolatedStorageSettings.ApplicationSettings.Save();
+                        //Navigate to the MainPage
+                        NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+                       
+                    }
+
+                }
+            }
         }
     }
 }
