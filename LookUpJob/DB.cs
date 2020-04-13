29a34f09b942b9d81e87f427a693a027f7bfae48
Felix <felix@rocketmail.com>
2020-04-14 00:07:26 +0300

    LINQ TO SQL database

--- LookUpJob/DB.cs
+++ LookUpJob/DB.cs
@@ -5,48 +5,278 @@ using System.Data.Linq.Mapping;
 public class User_details
 {
     [Column(IsDbGenerated = true, IsPrimaryKey = true)]
-    public int id
+    public int user_id
     {
         get;
         set;
     }
-    [Column]
+    [Column(DbType="varchar(100)", CanBeNull = false)]
     public string first_name
     {
         get;
         set;
     }
-    [Column]
+    [Column(DbType = "varchar(100)", CanBeNull = false)]
     public string last_name
     {
         get;
         set;
     }
-    [Column]
+    [Column(DbType = "varchar(5000)", CanBeNull = false)]
     public string email
     {
         get;
         set;
     }
-    [Column]
+    [Column(DbType = "bit", CanBeNull = false)]
     public bool is_Employee_or_JobSeeker
     {
         get;
         set;
     }
-    [Column]
+    [Column(DbType = "varchar(2000)", CanBeNull = false)]
     public string user_name
     {
         get;
         set;
     }
-    [Column]
+    [Column(DbType = "text", CanBeNull = false)]
     public string password
     {
         get;
         set;
     }
+
+    [Column(DbType = "bit", CanBeNull = true)]
+    public bool is_super_user
+    {
+        get;
+        set;
+    }
+
+    [Column(DbType = "bit", CanBeNull = true)]
+    public bool is_active
+    {
+        get;
+        set;
+    }
+
+    [Column(CanBeNull = true)]
+    public int company_id;
+    private EntityRef<Company> _CompanyUser;
+    [Association(Storage = "_CompanyUser", ThisKey = "company_id")]
+    public Company Company
+    {
+        get { return this._CompanyUser.Entity; }
+        set { this._CompanyUser.Entity = value; }
+    }
+
 }
+
+[Table]
+public class Vacancy
+{
+    [Column(IsDbGenerated = true, IsPrimaryKey = true)]
+    public int vacancies_id
+    {
+        get;
+        set;
+    }
+
+    [Column(DbType = "text", CanBeNull = false)]
+    public string short_description
+    {
+        get;
+        set;
+    }
+
+    [Column(DbType = "varchar(100)", CanBeNull = false)]
+    public string occupation
+    {
+        get;
+        set;
+    }
+
+    [Column(DbType = "varchar(100)", CanBeNull = false)]
+    public string position
+    {
+        get;
+        set;
+    }
+
+    [Column(CanBeNull = true)]
+    public int years_of_experience
+    {
+        get;
+        set;
+    }
+
+    [Column(DbType = "varchar(300)", CanBeNull = true)]
+    public string highest_level_of_education
+    {
+        get;
+        set;
+    }
+
+
+    [Column(DbType = "date", CanBeNull = false)]
+    public string vacancy_deadline_date
+    {
+        get;
+        set;
+    }
+
+    [Column(CanBeNull = false)]
+    public int user_id;
+    private EntityRef<User_details> _EmpoyeerVacancy;
+    [Association(Storage = "_EmployeeVacancy", ThisKey = "user_id")]
+    public User_details User_details
+    {
+        get { return this._EmpoyeerVacancy.Entity; }
+        set { this._EmpoyeerVacancy.Entity = value; }
+    }
+
+    [Column(DbType = "datetime", CanBeNull = true)]
+    public string publish_date
+    {
+        get;
+        set;
+    }
+
+}
+
+[Table]
+public class Company
+{
+    [Column(IsDbGenerated = true, IsPrimaryKey = true)]
+    public int company_id
+    {
+        get;
+        set;
+    }
+
+    [Column(DbType = "varchar(100) NOT NULL", CanBeNull = false)]
+    public string name
+    {
+        get;
+        set;
+    }
+
+    [Column(DbType = "varchar(100) NOT NULL", CanBeNull = false)]
+
+    public string company_type
+    {
+        get;
+        set;
+    }
+
+    [Column(DbType = "varchar(100) NOT NULL", CanBeNull = false)]
+    public string location
+    {
+        get;
+        set;
+    }
+
+    [Column(DbType = "varchar(5000) NOT NULL", CanBeNull = false)]
+    public string email
+    {
+        get;
+        set;
+    }
+}
+
+[Table]
+public class CV
+{
+    [Column(IsDbGenerated = true, IsPrimaryKey = true)]
+    public int cv_id
+    {
+        get;
+        set;
+    }
+
+    [Column(DbType = "text", CanBeNull = false)]
+    public string short_description
+    {
+        get;
+        set;
+    }
+
+
+    [Column(DbType = "varchar(100)", CanBeNull = false)]
+    public string occupation
+    {
+        get;
+        set;
+    }
+
+    [Column(CanBeNull = false)]
+    public int years_of_experience
+    {
+        get;
+        set;
+    }
+
+    [Column(DbType = "varchar(100)", CanBeNull = false)]
+    public string education_level
+    {
+        get;
+        set;
+    }
+
+    public int user_id;
+    private EntityRef<User_details> _EmployeeCV;
+    [Association(Storage = "_EmployeeCV", ThisKey = "user_id")]
+    public User_details User_details
+    {
+        get { return this._EmployeeCV.Entity; }
+        set { this._EmployeeCV.Entity = value; }
+
+    }
+
+}
+
+[Table]
+public class VacancyApplication
+{
+    [Column(IsDbGenerated = true, IsPrimaryKey = true)]
+    public int vacancy_application_id
+    {
+        set;
+        get;
+    }
+
+    [Column]
+    public int user_id;
+    private EntityRef<User_details> _Applicant;
+    [Association(Storage = "_Applicant", ThisKey= "user_id")]
+    public User_details User_details
+    {
+        get { return this._Applicant.Entity; }
+        set { this._Applicant.Entity = value; }
+    }
+
+    [Column]
+    public int cv_id;
+    private EntityRef<CV> _ApplicantCV;
+    [Association(Storage = "_ApplicantCV", ThisKey = "cv_id")]
+    public CV CV
+    {
+        get { return this._ApplicantCV.Entity; }
+        set { this._ApplicantCV.Entity = value; }
+    }
+
+    [Column]
+    public int vacancy_id;
+    private EntityRef<Vacancy> _Vacancy;
+    [Association(Storage = "_Vacancy", ThisKey = "vacancy_id")]
+    public Vacancy Vacancies
+    {
+        get { return this._Vacancy.Entity; }
+        set { this._Vacancy.Entity = value; }
+    }
+}
+
 
 public class UserDataContext : DataContext
 {
@@ -61,4 +291,38 @@ public class UserDataContext : DataContext
             return this.GetTable<User_details>();
         }
     }
+
+    public Table<Vacancy> Vacancies
+    {
+        get
+        {
+            return this.GetTable<Vacancy>();
+        }
+    }
+
+    public Table<Company> Company
+    {
+        get
+        {
+            return this.GetTable<Company>();
+        }
+    }
+
+    public Table<CV> CV
+    {
+        get
+        {
+            return this.GetTable<CV>();
+        }
+    }
+
+    public Table<VacancyApplication> VacancyApplications
+    {
+        get
+        {
+            return this.GetTable<VacancyApplication>();
+        }
+    }
+
+    
 }
