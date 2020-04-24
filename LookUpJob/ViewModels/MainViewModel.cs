d0e95a3423749381bd1922e94a327625443b2994
Felix <felix@rocketmail.com>
2020-04-24 22:44:16 +0300

    MainPage view models added

---
+++ LookUpJob/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+
+namespace LookUpJob.ViewModels
+{
+    public class MainViewModel 
+    {
+        public MainViewModel()
+        {
+            this.Items = new ObservableCollection<VacancyViewModel>();
+        }
+
+        public ObservableCollection<VacancyViewModel> Items { get; private set; }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        public void LoadData()
+        {
+            //Get data of db.Vacancy
+            using (UserDataContext udt = new UserDataContext(UserDataContext.DBConnectionString))
+            {
+                try
+                {
+                    IQueryable<Vacancy> query = from v in udt.Vacancies select v;
+                    IList<Vacancy> vacancies = query.ToList();
+
+                        foreach (Vacancy v in vacancies)
+                        {
+                            this.Items.Add(new VacancyViewModel() { ID = v.vacancies_id, ShortDescription = v.short_description, Position = v.position, HighestLevelOfEducation = v.highest_level_of_education });
+                        }
+                }
+                catch (Exception ex)
+                {
+                    //No Data to be displayed
+                    Console.WriteLine(ex);
+                }
+            }
+
+            this.IsDataLoaded = true;
+        }
+    }
+}
---
+++ LookUpJob/ViewModels/VacancyViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace LookUpJob.ViewModels
+{
+    public class VacancyViewModel : INotifyPropertyChanged
+    {
+        private int _id;
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                if(value !=_id)
+                {
+                    _id = value;
+                    INotifyPropertyChanged("ID");
+                }
+            }
+        }
+
+        private string _shortDescription;
+        public string ShortDescription
+        {
+            get
+            {
+                return _shortDescription;
+            }
+
+            set{
+                if(value !=_shortDescription)
+                {
+                    _shortDescription = value;
+                    INotifyPropertyChanged("ShortDescription");
+                }
+            }
+        }
+
+        private string _position;
+        public string Position
+        {
+            get
+            {
+                return _position;
+            }
+
+            set
+            {
+                if(value !=_position)
+                {
+                    _position = value;
+                    INotifyPropertyChanged("Position");
+                }
+            }
+        }
+
+        private int _yearsOfExperience;
+        public int YearsOfExperience
+        {
+            get
+            {
+                return _yearsOfExperience;
+            }
+
+            set
+            {
+                if(value !=_yearsOfExperience)
+                {
+                    _yearsOfExperience = value;
+                    INotifyPropertyChanged("YearOfExperience");
+                }
+            }
+        }
+
+        private string _vacancyDeadline;
+        public string VacancyDeadline
+        {
+            get 
+            {
+                return _vacancyDeadline;
+            }
+
+            set
+            {
+                if(value !=_vacancyDeadline)
+                {
+                    _vacancyDeadline = value;
+                    INotifyPropertyChanged("VacancyDeadline");
+                }
+            }
+        }
+
+        private string _highestLevelOfEducation;
+        public string HighestLevelOfEducation
+        {
+            get
+            {
+                return _highestLevelOfEducation;
+            }
+
+            set
+            {
+                if(value !=_highestLevelOfEducation)
+                {
+                    _highestLevelOfEducation = value;
+                    INotifyPropertyChanged("HighestLevelOfEducation");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void INotifyPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if(null !=handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
