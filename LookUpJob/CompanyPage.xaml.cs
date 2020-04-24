169549f20de0e463271d04f6eefc5e15f7ef67cc
Felix <felix@rocketmail.com>
2020-04-24 22:41:43 +0300

    Company page view

---
+++ LookUpJob/CompanyPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+
+namespace LookUpJob
+{
+    public partial class CompanyPage : PhoneApplicationPage
+    {
+        public CompanyPage()
+        {
+            InitializeComponent();   
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/VacanciesPage.xaml",UriKind.Relative));
+        }
+
+        
+
+    }
+}
