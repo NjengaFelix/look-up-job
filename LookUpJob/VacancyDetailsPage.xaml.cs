9b94ed36ef60d37243476b1db70adf4b0ad49160
Felix <felix@rocketmail.com>
2020-04-25 16:45:17 +0300

    Added vacancies detail page

---
+++ LookUpJob/VacancyDetailsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace LookUpJob
+{
+    public partial class VacancyDetailsPage : PhoneApplicationPage
+    {
+        public VacancyDetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        // When page is navigated to set data context to selected item in list
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (DataContext == null)
+            {
+                string selectedIndex = "";
+                if (NavigationContext.QueryString.TryGetValue("selectedItem", out selectedIndex))
+                {
+                    int index = int.Parse(selectedIndex);
+                    DataContext = App.ViewModel.Items[0];
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //get the vacancy_index
+
+        }
+    }
+}
