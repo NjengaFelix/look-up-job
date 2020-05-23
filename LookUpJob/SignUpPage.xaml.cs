8a3bce6ffc4d9623f92229e323ae349c251444d8
Felix <felix@rocketmail.com>
2020-05-23 18:46:32 +0300

    Added validation to the sign up

--- LookUpJob/SignUpPage.xaml.cs
+++ LookUpJob/SignUpPage.xaml.cs
@@ -7,11 +7,14 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Text.RegularExpressions;
+
 
 namespace LookUpJob
 {
     public partial class SignUpPage : PhoneApplicationPage
     {
+
         public SignUpPage()
         {
             InitializeComponent();
@@ -33,14 +36,29 @@ namespace LookUpJob
                 MessageBox.Show("Field first name is empty");
                 return;
             }
+            else if (!Regex.IsMatch(firstName, @"^[a-zA-Z]+$"))
+            {
+                MessageBox.Show("Field first name requires letters only");
+                return;
+            }
             else if (string.IsNullOrEmpty(lastName))
             {
                 MessageBox.Show("Field last name is empty");
                 return;
             }
+            else if (!Regex.IsMatch(lastName, @"^[a-zA-Z]+$"))
+            {
+                MessageBox.Show("Field last name requires letters only");
+                return;
+            }
             else if (string.IsNullOrEmpty(email))
             {
                 MessageBox.Show("Field email is empty");
+                return;
+            }
+            else if(!Regex.IsMatch(email, @"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$" ))
+            {
+                MessageBox.Show("Field email is invalid");
                 return;
             }
             else if (rdoEmployer.IsChecked == false && rdoJobSeeker.IsChecked == false)
@@ -53,11 +71,31 @@ namespace LookUpJob
                 MessageBox.Show("Field username is empty");
                 return;
             }
+            else if (!Regex.IsMatch(username, @".{4,}"))
+            {
+                MessageBox.Show("Username requires a minimum characters of 4");
+                return;
+            }
             else if (string.IsNullOrEmpty(password))
             {
                 MessageBox.Show("Field password is empty");
                 return;
             }
+            else if(!isPasswordValid(password))
+            {
+                return;
+            }
+            else if (string.IsNullOrEmpty(txtConfirmPass.Password))
+            {
+                MessageBox.Show("Field confirm password is empty");
+                return;
+            }
+            else if (!Regex.IsMatch(txtConfirmPass.Password, password))
+            {
+                MessageBox.Show("Field confirm password doesn't match field password!");
+                return;
+            }
+
             else
             {
                 //In the database, employee is tinyInt 1 while jobseeker is tinyInt 0
@@ -92,6 +130,40 @@ namespace LookUpJob
 
             
 
+        }
+
+
+        private bool isPasswordValid(string password)
+        {
+            bool isPasswordValid = false;
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMinimum8Chars = new Regex(@".{8,}");
+
+            if (!hasNumber.IsMatch(password))
+            {
+                MessageBox.Show("New password requires at least a number!");
+                txtPassword.Focus();
+                return isPasswordValid;
+            }
+            else if (!hasUpperChar.IsMatch(password))
+            {
+                MessageBox.Show("New password requires at least an uppercase!");
+                txtPassword.Focus();
+                return isPasswordValid;
+            }
+            else if (!hasMinimum8Chars.IsMatch(password))
+            {
+                MessageBox.Show("New password show have a minimum of 8 characters!");
+                txtPassword.Focus();
+                return isPasswordValid;
+            }
+            else
+            {
+                isPasswordValid = true;
+            }
+
+            return isPasswordValid;
         }
 
 
